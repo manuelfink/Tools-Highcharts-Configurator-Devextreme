c981fbc306eaf8cfb189fbf96a4b0e422f279f23
Manuel Fink <funnyprinter@googlemail.com>
2020-11-20 08:41:06 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,140 @@
+import React from "react";
+
+import DataGrid, {
+  Column,
+  Editing,
+  Paging,
+  Selection,
+  Lookup
+} from "devextreme-react/data-grid";
+import { Button } from "devextreme-react/button";
+import ColorBox from "devextreme-react/color-box";
+
+import ArrayStore from "devextreme/data/array_store";
+import DataSource from "devextreme/data/data_source";
+import { employees, states } from "./data.js";
+
+import { render } from "react-dom";
+import Highcharts from "highcharts/highstock";
+//import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
+const dataSource = new DataSource({
+  store: new ArrayStore({
+    data: employees,
+    key: "ID"
+  })
+});
+
+const columns = [
+  "name",
+  {
+    dataField: "color",
+    //dataType:"color"
+    dataType: "color",
+    editCellComponent: ColorBox
+  },
+  "lineWidth"
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedItemKeys: [],
+      chartOptions: {
+        chart: {
+          type: "spline"
+        },
+        title: {
+          text: "My chart"
+        },
+        series: [
+          {
+            name: "Series 1",
+            color: "#ff0000",
+            lineWidth: "5",
+            data: [1, 2, 1, 4, 3, 6]
+          }
+        ]
+      }
+    };
+
+    this.seriesConfig = this.state.chartOptions.series;
+
+    this.allowChartUpdate = true;
+
+    //this.selectionChanged = this.selectionChanged.bind(this);
+    this.updateChartSeries = this.updateChartSeries.bind(this);
+  }
+
+  render() {
+    const { chartOptions } = this.state;
+    return (
+      <div>
+        <div id="data-grid-demo">
+          <DataGrid
+            id="gridContainer"
+            dataSource={this.seriesConfig}
+            //keyExpr="ID"
+            showBorders={true}
+            //selectedRowKeys={this.state.selectedItemKeys}
+            //onSelectionChanged={this.selectionChanged}
+            defaultColumns={columns}
+          >
+            <Paging enabled={false} />
+            <Editing
+              mode="cell"
+              allowUpdating={true}
+              onChangesChange={this.updateChartSeries}
+            />
+          </DataGrid>
+        </div>
+        <div>
+          <Button
+            id="updateChart"
+            text="Update Chart"
+            height={34}
+            onClick={this.updateChartSeries}
+          />
+        </div>
+        <div>
+          <HighchartsReact
+            id="myGraph"
+            ref={"chartComponent"}
+            //allowChartUpdate={this.allowChartUpdate}
+            highcharts={Highcharts}
+            constructorType={"stockChart"}
+            options={chartOptions}
+          />
+        </div>
+      </div>
+    );
+  }
+  componentDidMount() {
+    const chart = this.refs.chartComponent.chart;
+  }
+  updateChartSeries() {
+    this.setState({
+      chartOptions: {
+        series: this.seriesConfig
+      }
+    });
+    //this.setState({chartOptions: this.state.chartOptions});
+    console.log(
+      "Highcharts Options",
+      this.state.chartOptions,
+      this.seriesConfig
+    );
+  }
+
+  /*selectionChanged(data) {
+    console.log(data);
+    return true;
+    this.setState({
+      selectedItemKeys: data.selectedRowKeys
+    });
+  }*/
+}
+
+export default App;
